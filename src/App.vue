f1d7ad4bea46e6f3e4f4b816a66aaa1aef5c2341
Daníel Örn Árnason <danielarnason85@gmail.com>
2021-11-11 13:22:10 +0100

    Maplibre gl kort virker

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <Kort />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Kort from '@/components/Kort.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Kort
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body {
+  height: 100%;
+  width: 100%;
+  margin: 0;
 }
 </style>
---
+++ src/components/Kort.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="mapcontainer"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue'
+import maplibregl, { Map } from 'maplibre-gl'
+
+export default defineComponent({
+    setup() {
+
+        const map = ref<Map>();
+
+        onMounted(() => {
+            map.value = new maplibregl.Map({
+                container: 'mapcontainer',
+                style: 'mapbox://styles/mapbox/light-v10',
+                center: [11.295584 ,55.341815],
+                zoom: 10,
+                accessToken: 'pk.eyJ1IjoiZGFuaWVsLWFybmFzb24iLCJhIjoiY2pyOHo2OXp3MGI2MDQ5dm1nMzZ0NjJycCJ9.ESsG_fdvITuPsMUbUevoGQ',
+            })
+            map.value.addControl(new maplibregl.NavigationControl(), 'top-left');
+        })
+
+        return {
+            map
+        }
+    },
+})
+</script>
+
+
+<style scoped>
+#mapcontainer {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+}
+</style>
