1e302dbb8e231be29faa8212fb8eef2450bb9f44
Daníel Örn Árnason <danielarnason85@gmail.com>
2021-11-17 15:10:02 +0100

    getRamdomLocations

--- src/components/Kort.vue
+++ src/components/Kort.vue
@@ -11,6 +11,22 @@ export default defineComponent({
 
         const map = ref<Map>();
 
+        const getRandomLocations = (features: [], n: number) => {
+            const result = new Array(n);
+            let len = features.length;
+            const taken = new Array(len);
+
+            if (n > len) {
+                throw new RangeError("getRandomLocation: more elements taken than available")
+            } 
+            while (n--) {
+                const x = Math.floor(Math.random() * len);
+                result[n] = features[x in taken ? taken[x] : x];
+                taken[x] = --len in taken ? taken[len] : len;
+            }
+            return result;
+        }
+
         onMounted(() => {
             map.value = new maplibregl.Map({
                 container: 'mapcontainer',
