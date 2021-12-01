a05d82e6e4c0577119701d0a8a5457d24dae39e9
Daníel Örn Árnason <danielarnason85@gmail.com>
2021-12-01 11:39:17 +0100

    Highscore css

--- src/components/Highscore.vue
+++ src/components/Highscore.vue
@@ -1,13 +1,13 @@
 <template>
     <div v-if="highscores.length > 0" id="highscore" class="mt-10 p-5 rounded-xl bg-gray-800 bg-opacity-50">
-        <h1 class="text-center text-3xl text-red-400 font-bold">Scoreboard!</h1>
+        <h1 class="text-center text-3xl text-red-400 font-bold mb-5">Top 10!</h1>
         <table>
             <tr class=" text-center font-bold border-b border-yellow-100">
                 <th class="px-3">Placering</th>
                 <th class="px-3">Navn</th>
                 <th class="px-3">Km forkert</th>
             </tr>
-            <tr class="text-center" v-for="(highscore, idx) in highscores" :key="highscore.id">
+            <tr class="text-center" v-for="(highscore, idx) in topTen" :key="highscore.id">
                 <td>{{ idx + 1 }}.</td>
                 <td>{{ highscore.name }}</td>
                 <td>{{ Math.round(highscore.score * 10) / 10 }}</td>
@@ -17,13 +17,16 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue'
+import { computed, defineComponent, onMounted, ref } from 'vue'
 import { Highscore } from "@/highscoreInterface";
 import { supabase } from "@/supabase";
 
 export default defineComponent({
     setup() {
         const highscores = ref<Highscore[]>([]);
+        const topTen = computed(() => {
+            return highscores.value.slice(0, 10)
+        })
         
         onMounted( async () => {
             const response = await supabase
@@ -35,7 +38,8 @@ export default defineComponent({
         })
 
         return {
-            highscores
+            highscores,
+            topTen
         }
     },
 })
