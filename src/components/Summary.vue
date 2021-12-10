b793212415b3e70e37a8fadd8d5f913773544c2e
Daníel Örn Árnason <danielarnason85@gmail.com>
2021-12-10 14:38:54 +0100

    nu med konfetti

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -8,7 +8,8 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, PropType } from 'vue'
+import { computed, defineComponent, onMounted, PropType } from 'vue'
+import confetti from 'canvas-confetti'
 
 export default defineComponent({
     props: {
@@ -31,6 +32,13 @@ export default defineComponent({
         const playAgain = () => {
             emit('playAgain');
         }
+
+        onMounted(() => {
+            confetti({
+                particleCount: 300,
+                spread: 125
+            })
+        })
 
         return {
             roundedDistance,
