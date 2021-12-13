0cbf64c3400f2164e143a4faedfa21900fcd0d43
Daníel Örn Árnason <danielarnason85@gmail.com>
2021-12-13 14:26:11 +0100

    klar til deploy

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:danielarnason/geoquiz_slagelse.git master:gh-pages
+git push -f git@github.com:danielarnason/geoquiz_vd.git master:gh-pages
 
 cd -
